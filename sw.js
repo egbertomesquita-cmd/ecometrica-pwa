0a310e17c64f68fc5adad7e2c65a17ee828956ea
egbertomesquita-cmd <egbertomesquita@uern.br>
2026-08-18 16:48:37 -0300

    chore: refresh PWA cache to v20

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "ecometrica-v19";
-const ASSETS = ["./", "./index.html", "./styles.css", "./footer.css?v=6", "./search.css?v=12", "./map.css?v=15", "./app.js?v=19", "./manifest.webmanifest", "./icons/icon.svg", "./icons/decb-uern.png", "./icons/flag-br.svg"];
+const CACHE = "ecometrica-v20";
+const ASSETS = ["./", "./index.html", "./styles.css", "./footer.css?v=6", "./search.css?v=12", "./map.css?v=15", "./protocol.css?v=20", "./app.js?v=20", "./manifest.webmanifest", "./icons/icon.svg", "./icons/decb-uern.png", "./icons/flag-br.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
