887696f704155378887ed77d765174805d49b751
egbertomesquita-cmd <egbertomesquita@uern.br>
2026-08-18 14:02:36 -0300

    Garante atualização imediata da PWA em sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = "ecometrica-v5";
-const ASSETS = ["./", "./index.html", "./styles.css", "./footer.css", "./app.js", "./manifest.webmanifest", "./icons/icon.svg", "./icons/decb-uern.png"];
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+const CACHE = "ecometrica-v6";
+const ASSETS = ["./", "./index.html", "./styles.css", "./footer.css?v=6", "./app.js?v=6", "./manifest.webmanifest", "./icons/icon.svg", "./icons/decb-uern.png"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(fetch(event.request).then(response => {
