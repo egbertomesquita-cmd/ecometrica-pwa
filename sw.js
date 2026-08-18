dd4c1fea08e62f1a96819ef3333e3ebbeeab94c0
egbertomesquita-cmd <egbertomesquita@uern.br>
2026-08-18 15:16:51 -0300

    Adiciona filtro geográfico guiado

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "ecometrica-v10";
-const ASSETS = ["./", "./index.html", "./styles.css", "./footer.css?v=6", "./search.css?v=10", "./app.js?v=10", "./manifest.webmanifest", "./icons/icon.svg", "./icons/decb-uern.png"];
+const CACHE = "ecometrica-v11";
+const ASSETS = ["./", "./index.html", "./styles.css", "./footer.css?v=6", "./search.css?v=11", "./app.js?v=11", "./manifest.webmanifest", "./icons/icon.svg", "./icons/decb-uern.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
