689adb30e019243b4142163c95c33a9d7a66f844
egbertomesquita-cmd <egbertomesquita@uern.br>
2026-08-18 14:49:21 -0300

    Remove credenciais OAuth da URL em sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "ecometrica-v7";
-const ASSETS = ["./", "./index.html", "./styles.css", "./footer.css?v=6", "./app.js?v=7", "./manifest.webmanifest", "./icons/icon.svg", "./icons/decb-uern.png"];
+const CACHE = "ecometrica-v8";
+const ASSETS = ["./", "./index.html", "./styles.css", "./footer.css?v=6", "./app.js?v=8", "./manifest.webmanifest", "./icons/icon.svg", "./icons/decb-uern.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
