579e3264edd04c5bbbbba75f1b17370009eae618
José Egberto Mesquita Pinto Júnior <egbertomesquita-cmd@users.noreply.github.com>
2026-09-01 18:44:51 -0300

    Evita corte do diagrama PRISMA no PDF

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = "ecometrica-v61";
+const CACHE = "ecometrica-v62";
 const APP_SHELL = "./index.html";
 const ASSETS = [
   "./",
   APP_SHELL,
-  "./styles.css?v=61",
+  "./styles.css?v=62",
   "./footer.css?v=8",
   "./search.css?v=13",
   "./map.css?v=15",
@@ -14,7 +14,7 @@ const ASSETS = [
   "./flow.css?v=37",
   "./help.css?v=55",
   "./topbar.css?v=55",
-  "./app.js?v=61",
+  "./app.js?v=62",
   "./manifest.webmanifest",
   "./icons/icon.svg",
   "./icons/decb-uern.png",
