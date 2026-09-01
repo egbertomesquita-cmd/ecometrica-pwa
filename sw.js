6b53ea4c47de7e55ba5d5c1827e3595f05ffca03
José Egberto Mesquita Pinto Júnior <egbertomesquita-cmd@users.noreply.github.com>
2026-09-01 18:18:30 -0300

    Corrigir busca SciELO com resultado zero

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = "ecometrica-v58";
+const CACHE = "ecometrica-v59";
 const APP_SHELL = "./index.html";
 const ASSETS = [
   "./",
   APP_SHELL,
-  "./styles.css?v=58",
+  "./styles.css?v=59",
   "./footer.css?v=8",
   "./search.css?v=13",
   "./map.css?v=15",
@@ -14,7 +14,7 @@ const ASSETS = [
   "./flow.css?v=37",
   "./help.css?v=55",
   "./topbar.css?v=55",
-  "./app.js?v=58",
+  "./app.js?v=59",
   "./manifest.webmanifest",
   "./icons/icon.svg",
   "./icons/decb-uern.png",
