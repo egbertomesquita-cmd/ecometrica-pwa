f30bf9f9b7ca90a038e7462b8ea58ac008aeb447
egbertomesquita-cmd <egbertomesquita@uern.br>
2026-09-01 18:22:55 -0300

    Corrige filtro de período da SciELO na versão 60 (sw.js)

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = "ecometrica-v59";
+const CACHE = "ecometrica-v60";
 const APP_SHELL = "./index.html";
 const ASSETS = [
   "./",
   APP_SHELL,
-  "./styles.css?v=59",
+  "./styles.css?v=60",
   "./footer.css?v=8",
   "./search.css?v=13",
   "./map.css?v=15",
@@ -14,7 +14,7 @@ const ASSETS = [
   "./flow.css?v=37",
   "./help.css?v=55",
   "./topbar.css?v=55",
-  "./app.js?v=59",
+  "./app.js?v=60",
   "./manifest.webmanifest",
   "./icons/icon.svg",
   "./icons/decb-uern.png",
