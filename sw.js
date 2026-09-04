92034294d31e484f633c8d1af610a328735feedf
José Egberto Mesquita Pinto Júnior <egbertomesquita-cmd@users.noreply.github.com>
2026-09-03 21:50:42 -0300

    Adiciona Lei de Zipf à análise cienciométrica

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ecometrica-v74";
+const CACHE = "ecometrica-v75";
 const APP_SHELL = "./index.html";
 const ASSETS = [
   "./",
@@ -11,11 +11,11 @@ const ASSETS = [
   "./dedup.css?v=22",
   "./prisma.css?v=64",
   "./extraction.css?v=66",
-  "./analytics.css?v=74",
+  "./analytics.css?v=75",
   "./flow.css?v=64",
   "./help.css?v=69",
   "./topbar.css?v=69",
-  "./app.js?v=74",
+  "./app.js?v=75",
   "./manifest.webmanifest",
   "./icons/icon.svg",
   "./icons/decb-uern.png",
