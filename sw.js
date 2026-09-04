7333d263782142c86e49ff3da8b146801cdb084f
José Egberto Mesquita Pinto Júnior <egbertomesquita-cmd@users.noreply.github.com>
2026-09-03 21:56:58 -0300

    Adiciona indicadores de impacto e índice h

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ecometrica-v75";
+const CACHE = "ecometrica-v76";
 const APP_SHELL = "./index.html";
 const ASSETS = [
   "./",
@@ -11,11 +11,11 @@ const ASSETS = [
   "./dedup.css?v=22",
   "./prisma.css?v=64",
   "./extraction.css?v=66",
-  "./analytics.css?v=75",
+  "./analytics.css?v=76",
   "./flow.css?v=64",
   "./help.css?v=69",
   "./topbar.css?v=69",
-  "./app.js?v=75",
+  "./app.js?v=76",
   "./manifest.webmanifest",
   "./icons/icon.svg",
   "./icons/decb-uern.png",
